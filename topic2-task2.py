d6e2bbf1c1a50ee0f6b546b558b50a23191e9b3c
Akshtha14 <23f2001878@ds.study.iitm.ac.in>
2025-05-20 16:32:28 +0530

    Create topic2-task2.py

---
+++ topic2-task2.py
@@ -0,0 +1,11 @@
+# Input
+num1 = float(input())
+num2 = float(input())
+
+# Comparison
+if num1 > num2:
+    print("The first number is greater than the second.")
+elif num1 < num2:
+    print("The first number is less than the second.")
+else:
+    print("Both numbers are equal.")
