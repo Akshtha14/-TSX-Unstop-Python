529bf8549477998622ea0a18789a30d8e8b41dd2
Akshtha14 <23f2001878@ds.study.iitm.ac.in>
2025-05-20 16:33:46 +0530

    Create topic2-task4.py

---
+++ topic2-task4.py
@@ -0,0 +1,15 @@
+# Arithmetic
+a = 10
+b = 3
+print("Addition:", a + b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+
+# Logical
+x = True
+y = False
+print("x and y:", x and y)
+print("x or y:", x or y)
+print("not x:", not x)
