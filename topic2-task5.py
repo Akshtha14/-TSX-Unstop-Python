fc70deec70f33374666bee19e58e06ecff75abfb
Akshtha14 <23f2001878@ds.study.iitm.ac.in>
2025-05-20 16:40:46 +0530

    Create topic2-task5.py

---
+++ topic2-task5.py
@@ -0,0 +1,9 @@
+# Input
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+
+# Concatenation
+result = str1 + str2
+
+# Output
+print("Concatenated string:", result)
