6e0013b68582ef0e7ba21f8dc651b746006d4daf
Akshtha14 <23f2001878@ds.study.iitm.ac.in>
2025-05-20 16:31:06 +0530

    Create topic2-task1.py

---
+++ topic2-task1.py
@@ -0,0 +1,10 @@
+length = 5 # Rectangle dimensions
+width = 3 # Rectangle dimensions
+
+
+area = length * width #formulas
+perimeter = 2 * (length + width) #formulas
+
+
+print("Area of the rectangle:", area) #output
+print("Perimeter of the rectangle:", perimeter) #output
